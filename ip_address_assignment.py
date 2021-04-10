f0aff626bf5d04d0d0521eeb3b97109878b4e60c
Brett <brp5088@rit.edu>
2021-04-09 20:10:46 -0400

    added new functions. fixed a few bugs. Starting to work on generating the table

--- ip_address_assignment.py
+++ ip_address_assignment.py
@@ -14,6 +14,8 @@ bitPattern = {
 }
 
 def decimal_to_binary( num ):
+    assert num <= 255, f"Shouldn't be bigger than 255. tried converting { num }"
+
     string = ''
     if type( num ) == str:
         num = int( num )
@@ -21,6 +23,9 @@ def decimal_to_binary( num ):
 
     map = {}
     last_used = None
+
+    if num == 0:
+        return "0" * 8
 
     while remainder >= 0:
         if last_used == -1:
@@ -41,50 +46,73 @@ def decimal_to_binary( num ):
 
         last_used -= 1
 
-        if len( string ) < 8:
-            string = "0" * ( 8 - len( string ) ) + string
-
-        if len( string ) > 8:
-            string = string[ ( 8 - len( string ) ) + 1: ]
+    if len( string ) < 8:
+        string = "0" * ( 8 - len( string ) ) + string
 
     return string
+
+def binary_to_decimal( string ):
+    total = 0
+
+    string = [ x for x in reversed( string ) ]
+
+    index = 0
+
+    while index < len( string ):
+
+        # print( index )
+        if string[ index ] == "1":
+            total = total + 2**( index )
+        index = index + 1
+
+    return total
+
+
 
 
 
 class ipAddress:
     def __init__(self, oct1, oct2, oct3, oct4, mask=False, hosts=None ):
-        self.oct1 = oct1
-        self.oct2 = oct2
-        self.oct3 = oct3
-        self.oct4 = oct4
+        self.oct1 = oct1 if type( oct1 ) == str else decimal_to_binary( oct1 )
+        self.oct2 = oct2 if type( oct2 ) == str else decimal_to_binary( oct2 )
+        self.oct3 = oct3 if type( oct3 ) == str else decimal_to_binary( oct3 )
+        self.oct4 = oct4 if type( oct4 ) == str else decimal_to_binary( oct4 )
 
-        self.mask = None
+        self.CIDR = None
+        self.subnetMask = None
 
-        self.CIDR = 32 - ( ceil( log2( hosts ) ) ) 
 
-        # if mask:
-        #     self.subnetMask = self.generateSubnetMask()
+        if mask:
+            self.CIDR = 32 - ( ceil( log2( hosts ) ) ) 
+            self.subnetMask = self.generateSubnetMask()
 
     def __str__(self):
-        return f"{ self.oct1 }.{ self.oct2 }.{ self.oct3 }.{ self.oct4 }"
+        return f"{ binary_to_decimal( self.oct1 ) }.{ binary_to_decimal( self.oct2 ) }.{ binary_to_decimal( self.oct3 ) }.{ binary_to_decimal( self.oct4 ) }"
+
 
     def generateSubnetMask(self):
 
         lst = []
-
         index = self.CIDR
 
         while index > 0:
 
             if index - 8 > 0:
-                lst.append( 255 )
-                index = index - 8
+                lst.append( decimal_to_binary( 255 ) )
             else:
+                tmp = "1" * index + "0" * ( 8 - index)
+                lst.append( decimal_to_binary( bitPattern[ tmp ] ) )
+            
+            index = index - 8
 
+        if len( lst ) < 4:
+            lst.append( decimal_to_binary( 0 ) )
 
+        # return None
+        return ipAddress( bitPattern[ lst[0] ], bitPattern[ lst[1] ], bitPattern[ lst[2] ], bitPattern[ lst[3] ] ) 
+        
+        
 
-
-        return ipAddress( bitPattern[ decimal_to_binary( self.oct1 ) ], bitPattern[ decimal_to_binary( self.oct2 ) ], bitPattern[ decimal_to_binary( self.oct3 ) ], bitPattern[ decimal_to_binary( self.oct4 ) ] ) 
 
 
 
@@ -92,20 +120,20 @@ class ipAddress:
 
 def generate_table( deptLst, ipAddress):
     deptLst = sorted( deptLst )
-    
     print( deptLst )
+
+    
 
 
 
 def main():
-    deptLst = [ ('A', 500), ('B', 205), ('C', 100), , ('D', 80) ]
+    deptLst = [ ('A', 500), ('B', 205), ('C', 100), ('D', 80) ]
 
-
-    address = ipAddress( 156, 31, 28, 0, True, 500 )
-
-    print( address )
+    
+    # address = ipAddress( 156, 31, 28, 0, True, host=500 )
+    # print( address )
     # print( address.subnetMask )
-    generate_table( deptLst, ipAddress( 156, 31, 28, 0) )
+    generate_table( deptLst, ipAddress( 156, 31, 28, 0, host=500) )
 
 
 
